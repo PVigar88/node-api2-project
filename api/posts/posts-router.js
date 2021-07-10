bb1c5fb5aa1fb58b630975695d0bf53687302f2b
PVigar88 <Pvigar@gmail.com>
2021-07-10 19:34:03 -0400

    4 endpoints complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,6 +1,7 @@
 // implement your posts router here
 
 const express = require("express");
+const dogModel = require("../../../node-api1-guided/api/dog-model.js");
 
 const Post = require("./posts-model.js");
 
@@ -34,6 +35,46 @@ router.get("/:id", (req, res) => {
       console.log(err);
       res.status(500).json({
         message: "The post information could not be retrieved",
+      });
+    });
+});
+
+router.post("/", (req, res) => {
+  const newPost = req.body;
+
+  if (!newPost.title || !newPost.contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Post.insert(req.body)
+      .then((post) => {
+        res.status(201).json(post);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+        });
+      });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  Post.update(req.params.id, req.body)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "The post information could not be modified",
       });
     });
 });
