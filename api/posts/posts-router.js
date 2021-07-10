cbb5e9f88ed2619ef0b050107f6b81ce3b26b9cd
PVigar88 <Pvigar@gmail.com>
2021-07-10 19:02:57 -0400

    posts-router get end points complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -18,3 +18,22 @@ router.get("/", (req, res) => {
         .json({ message: "The posts information could not be retrieved" });
     });
 });
+
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "The post information could not be retrieved",
+      });
+    });
+});
