be3346299220c3574dd0fec6a5b3a9801e79d2cb
PVigar88 <Pvigar@gmail.com>
2021-07-15 00:31:20 -0400

    added dot env and variable port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
+
 // require your server and launch it here
 const server = require("./api/server");
+const port = process.env.PORT || 5000;
 
-server.listen(4000, () => {
-  console.log("server is listening on http://localhost:4000");
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
